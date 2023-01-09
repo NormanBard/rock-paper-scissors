99e75efc84e90d578b1a005ee2bfae082107fe8b
NormanBard <106641748+NormanBard@users.noreply.github.com>
2023-01-09 16:56:01 -0500

    Add js file

---
+++ javascript.js
@@ -0,0 +1,18 @@
+function game()
+//play the game
+//play the five rounds
+//console based
+
+function playRound(playerSelection, computerSelection){
+    const playerSelection =
+
+
+}
+ 
+ function playerChoice (){
+    //get input from player
+ }
+
+ function computerSelection(){
+    //get input from computer
+ }
